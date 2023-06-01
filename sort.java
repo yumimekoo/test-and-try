a8338047f2f660fd75f776826b91171c20e2b5b5
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2023-06-01 15:07:51 +0200

    01_06_23
    Trying out sort algorithm because my friend wanted to do also and we wanted to compare our algorithms!

---
+++ sort.java
@@ -0,0 +1,39 @@
+int [] sort(int[] a){
+int c = 0;
+int d = 0;
+int counter = 1;
+boolean flag = false;
+int [] sum = a;
+while(flag==false){
+    for (int i = 0; i < (a.length)-1;i++){
+        c = sum[i+1];
+        d = sum[i];
+        if(d>c){
+            sum[i] = c; 
+            sum [i+1] = d;
+            flag = false;
+        } else {
+            sum[i] = d;
+            counter++;
+        }
+        if(counter==a.length-1){
+            flag=true;
+        } 
+    }
+    print(sum);
+    counter = 0;   
+}
+return sum; 
+}
+
+// visual purposes only //
+void print(int [] a){
+    for (int i = 0; i < a.length;i++){
+        if (i != a.length-1){
+        System.out.print(a[i]+", ");
+        } else {
+        System.out.print(a[i]);
+        System.out.println();
+        } 
+    }
+}
